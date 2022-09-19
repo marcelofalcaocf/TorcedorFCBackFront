c623bfa41bc3d7d8175313299afbe25d4e9fff6f
Marcelo Falcão <marcelo_falcaoc@hotmail.com>
2022-09-19 15:05:01 -0300

    Adicao das telas de Jefinho

---
+++ TorcedorFCBackfront/Tela1ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  projectAppFutebol
+//
+//  Created by JEFERSON FERREIRA on 21/08/22.
+//
+
+import UIKit
+
+class Tela1ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Tela1ViewController-viewDidLoad")
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        print("Tela1ViewController-viewDidApper")
+    }
+
+        
+        
+}
+
