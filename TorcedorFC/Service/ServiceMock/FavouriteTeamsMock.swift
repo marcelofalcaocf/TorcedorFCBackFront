4e67ba8bba0147c9c787c04252c9ac0410278b38
KarinaMonteiro <karina.kk93@gmail.com>
2022-10-29 23:20:37 -0300

    Criando mock da tela Times Favoritos

---
+++ TorcedorFC/Service/ServiceMock/FavouriteTeamsMock.swift
@@ -0,0 +1,113 @@
+//
+//  FavouriteTeamsMock.swift
+//  TorcedorFC
+//
+//  Created by Tiago & Karina on 29/10/22.
+//
+
+import Foundation
+
+class FavouriteTeamsMock {
+    var teams: [Time] = [
+        Time(
+            timeId: 1,
+            nomePopular: "Palmeiras",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/7spurne-xDt2p6C0imYYNA_48x48.png"
+        ),
+        Time(
+            timeId: 2,
+            nomePopular: "Internacional",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/OWVFKuHrQuf4q2Wk0hEmSA_48x48.png"
+        ),
+        Time(
+            timeId: 3,
+            nomePopular: "Flamengo",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/orE554NToSkH6nuwofe7Yg_48x48.png"
+        ),
+        Time(
+            timeId: 4,
+            nomePopular: "Corinthians",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/tCMSqgXVHROpdCpQhzTo1g_48x48.png"
+        ),
+        Time(
+            timeId: 5,
+            nomePopular: "Fluminense",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/fCMxMMDF2AZPU7LzYKSlig_48x48.png"
+        ),
+        Time(
+            timeId: 6,
+            nomePopular: "Athletico-PR",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/LtpA9v-CQ_Qs2bgWVdDXxQ_48x48.png"
+        ),
+        Time(
+            timeId: 7,
+            nomePopular: "Atlético-MG",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/q9fhEsgpuyRq58OgmSndcQ_48x48.png"
+        ),
+        Time(
+            timeId: 8,
+            nomePopular: "América-MG",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/ZFsHqGsKNQPlCQjZ4Lu-EQ_48x48.png"
+        ),
+        Time(
+            timeId: 9,
+            nomePopular: "São Paulo",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/4w2Z97Hf9CSOqICK3a8AxQ_48x48.png"
+        ),
+        Time(
+            timeId: 10,
+            nomePopular: "Fortaleza",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/me10ephzRxdj45zVq1Risg_48x48.png"
+        ),
+        Time(
+            timeId: 11,
+            nomePopular: "Botafogo",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/KLDWYp-H8CAOT9H_JgizRg_48x48.png"
+        ),
+        Time(
+            timeId: 12,
+            nomePopular: "Santos",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/VHdNOT6wWOw_vJ38GMjMzg_48x48.png"
+        ),
+        Time(
+            timeId: 13,
+            nomePopular: "Bragantino",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/lMyw2zn1Z4cdkaxKJWnsQw_48x48.png"
+        ),
+        Time(
+            timeId: 14,
+            nomePopular: "Goiás",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/gb8bo2x00XsbvsVp9nGniA_48x48.png"
+        ),
+        Time(
+            timeId: 15,
+            nomePopular: "Coritiba",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/LaFlBADLmsjHfGTehCYtuA_48x48.png"
+        ),
+        Time(
+            timeId: 16,
+            nomePopular: "Ceará SC",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/mSl0cz3i2t8uv4zcprobOg_48x48.png"
+        ),
+        Time(
+            timeId: 17,
+            nomePopular: "Cuiába",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/j6U8Rgt_6yyf0Egs9nREXw_48x48.png"
+        ),
+        Time(
+            timeId: 18,
+            nomePopular: "Atlético-GO",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/9mqMGndwoR9og_Z0uEl2kw_48x48.png"
+        ),
+        Time(
+            timeId: 19,
+            nomePopular: "Avaí",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/9cwCmoBXGaPJ_Q5cgUeocg_48x48.png"
+        ),
+        Time(
+            timeId: 20,
+            nomePopular: "Juventude",
+            escudo: "https://ssl.gstatic.com/onebox/media/sports/logos/JrXw-m4Dov0gE2Sh6XJQMQ_48x48.png"
+        )
+    ]
+}
