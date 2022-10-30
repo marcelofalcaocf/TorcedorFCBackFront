74660ef25ea41a364479a54ac177e9f67cf4f71f
KarinaMonteiro <karina.kk93@gmail.com>
2022-10-29 22:32:31 -0300

    Criando mock da tela home

---
+++ TorcedorFC/Service/ServiceMock/HomeMock.swift
@@ -0,0 +1,25 @@
+//
+//  HomeMock.swift
+//  TorcedorFC
+//
+//  Created by Tiago & Karina on 29/10/22.
+//
+
+import Foundation
+
+class HomeMock {
+    var campeonatos: [Campeonato] = [
+        Campeonato(
+            campeonatoId: 1,
+            logo: "https://api.api-futebol.com.br/images/competicao/brasileiro-seriea.png",
+            nomePopular: "Brasileirão",
+            edicaoAtual: Edicao(edicaoId: 1, temporada: "2022")
+        ),
+        Campeonato(
+            campeonatoId: 2,
+            logo: "https://api.api-futebol.com.br/images/competicao/copa-do-brasil.png",
+            nomePopular: "Copa do Brasil",
+            edicaoAtual: Edicao(edicaoId: 2, temporada: "2022")
+        )
+    ]
+}
