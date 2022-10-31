395168bc1116d496b0d2049d81afcd3548b1eeed
KarinaMonteiro <karina.kk93@gmail.com>
2022-10-30 21:27:19 -0300

    ajustes

--- TorcedorFC/Utils/AppDelegate.swift
+++ TorcedorFC/Utils/AppDelegate.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 import CoreData
-//import FirebaseCore
+import FirebaseCore
 
 
 @main
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        //FirebaseApp.configure()
+        FirebaseApp.configure()
         return true
     }
 
